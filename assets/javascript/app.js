9ba71741ed931a9cda94324c9c2a69228329a372
Sum Guy <gjserq@gmail.com>
2019-01-24 16:37:43 -0700

    add files

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+//declaring variables
+
+    var qs = [
+    { q: "In The Simpsons episode, 'Bart Gets an F', Lisa has the mumps",a: "t" },
+    { q: "Dave Grohl drums for the Foo Fighters", a: "f"},
+    { q: "Touch is the sense most commonly linked with memory", a: "f"},
+    { q: "A kilogram is the weight of a litre of water", a: "t"},
+    { q: "Rabbits are raised in a warren.", a: "t"}
+    { q: "Pluto is a planet", a: "t"}
+
+    ]
+    
+    var score = 0;
+    var questionIndex = 0;
+
+    function renderQuestion
+
+
+
+
+
+
+})
